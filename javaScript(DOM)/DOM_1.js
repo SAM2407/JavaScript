40335506ed3d53f66c8ce7ae961a03d03edba9be
Samman Gond <112507091+SAM2407@users.noreply.github.com>
2025-05-28 22:28:35 +0530

    DOM_1.js

--- javaScript(DOM)/DOM_1.js
+++ javaScript(DOM)/DOM_1.js
@@ -1,1 +1,16 @@
-
+//DOM stands for document object model :- it is a tree like structure 
+//char/header->tags->token->nodes->DOM
+//we can access any element by document.getElementbyId() it uses the id of the html tag
+//it always give the single object if we want multiple obj then we have to use 
+//"document.getElementsByClassName()" it access the element by class name of html tag
+//they all return Html collection which is array like obj not array
+//document.getElementsByTagName()
+//Query selector('#header');it select elemnt like we use to do in css 
+//Query selector('.header');its slect elemetn by className
+//Query selector('#\header');its select with tag name;
+// document.querySelectorAll("className"); for selecting more than one element at a time
+//updating existing content-
+//innerHtmL- we can set an element of html content  and get an elemets or we can get all the decentdent of that element.it also render the elemnet of the next element
+//  outerHTML:-Refers to the entire element including its tags.You can get or set the whole element.
+//  textContent:- we can set the textual content of html and set the textual content also 
+//  innerText :- 
